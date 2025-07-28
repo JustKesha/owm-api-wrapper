cfee2cf5c1dd0f36d77f64da8e9eb6fe609bce94
kesha <73757143+JustKesha@users.noreply.github.com>
2025-07-28 19:26:19 +0400

    Fix main

--- main.py
+++ main.py
@@ -1,65 +1,40 @@
 import os
-
 import asyncio
 import nest_asyncio
 import dotenv
-
 import tests
 import geocode
 import weather
-import bot
 from tests.logger import MessageFilters
 
-def configurate():
-    print('/ configuring project')
-
+def init():
     dotenv.load_dotenv()
-
     weather.configurate(
-        base_url=os.getenv('OPENWEATHER_BASE_URL'),
-        key=os.getenv('OPENWEATHER_API_KEY') )
-    
-    geocode.configurate(user_agent=os.getenv('USER_AGENT'))
+        base_url = os.getenv('OPENWEATHER_BASE_URL'),
+        key = os.getenv('OPENWEATHER_API_KEY')
+        )
+    geocode.configurate(user_agent = os.getenv('USER_AGENT'))
 
-    test_guild = os.getenv('DISCORD_TEST_GUILD_ID')
-
-    bot.configurate(
-        api_key=os.getenv('DISCORD_API_KEY'),
-        test_guild_ids=[test_guild] if test_guild else None )
-
-async def run_tests(log_filter:int=MessageFilters.ONLY_RESULTS) -> bool:
-    print('/ running basic module tests')
-
+async def test(log_filter: int = MessageFilters.ONLY_RESULTS) -> bool:
     tests.logger.set_filter(log_filter)
-    
     return await tests.run_tests()
 
-def run_bot(log_filter:list=bot.log.Filters.NONE):
-    print('/ starting the bot')
+async def main(
+        run_tests: bool = False,
+        logger_mode: MessageFilters = MessageFilters.ONLY_RESULTS
+        ):
+    init()
 
-    bot.log.set_filter(log_filter)
+    if run_tests and not await test(logger_mode):
+        return print("ERR Some of the tests failed")
     
-    try:
-        bot.start()
-    except Exception as e:
-        raise e
-
-async def main(include_tests:bool=False):
-    print('/ starting')
-
-    configurate()
-
-    if include_tests and not await run_tests():
-        return
-
-    try:
-        run_bot()
-    except Exception as e:
-        print('! something went wrong:', e)
-        return
+    # EXAMPLE USAGE
+    where = input("Enter location name: ")
+    place = geocode.get_location(where)
+    weath = await weather.get_weather(place.lat, place.lon)
+    print(weath)
 
 if __name__ ==  '__main__':
     nest_asyncio.apply()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete( main() )
-    
+    loop.run_until_complete( main(run_tests = False) )
